c375d4091893efa33702e82b25f96771e88f7c59
Ayush Jaiswal <ayushjaiswal9099@gmail.com>
2026-08-20 17:22:21 +0530

    Passing data through array or accessing through index

--- 04-card-projects/src/App.jsx
+++ 04-card-projects/src/App.jsx
@@ -2,10 +2,13 @@
 import User from "./components/User";
 
 const App = () => {
+
+  const arr = [10,20,30,40]
+
   return (
     <div className="parent">
-       <User name="Ayush"/>
-       <User name="Akash"/>
+       <User name={arr[0]}/>
+       <User name={arr[1]}/>
     </div>
   );
 };
