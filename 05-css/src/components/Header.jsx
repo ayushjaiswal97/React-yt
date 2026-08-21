9268da1b88508a4161df70c4327d99560fa8a09b
Ayush Jaiswal <ayushjaiswal9099@gmail.com>
2026-08-21 20:22:41 +0530

    Styling css with specific file

---
+++ 05-css/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/Header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>
+     <h1>Sheryians</h1>
+     <button className='btn'>Login</button>
+    </div>
+  )
+}
+
+export default Header
